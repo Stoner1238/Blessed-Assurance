7d3b1dbb2f9aa2705d1b6c544cf2d0c6d1410299
Aregbe Ayatulah Adekunle <144852736+Stoner1238@users.noreply.github.com>
2026-03-14 20:00:22 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+function reveal(){
+
+var reveals = document.querySelectorAll(".reveal");
+
+for(var i = 0; i < reveals.length; i++){
+
+var windowHeight = window.innerHeight;
+var elementTop = reveals[i].getBoundingClientRect().top;
+var elementVisible = 100;
+
+if(elementTop < windowHeight - elementVisible){
+reveals[i].classList.add("active");
+}
+
+}
+
+}
+
+window.addEventListener("scroll", reveal);
+
+// MOBILE MENU
+
+const toggle = document.querySelector(".menu-toggle");
+const nav = document.querySelector(".nav-links");
+
+toggle.addEventListener("click", () => {
+nav.classList.toggle("active");
+});
